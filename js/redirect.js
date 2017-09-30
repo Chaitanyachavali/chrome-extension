362551774385aea2352324f17adbd12b853ce3f3
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2017-09-30 23:53:38 +0530

    removing examples and fixing the repo

--- js/redirect.js
+++ js/redirect.js
@@ -1,4 +1,1 @@
-$(document).ready(function() {
-	// chrome.tabs.create({active: true, url: "http://google.com"});
-	chrome.tabs.create({'url': chrome.extension.getURL('mytab.html'), 'active': true});
-});
+chrome.browserAction.onClicked.addListener(function(tab) { chrome.tabs.create({'url': chrome.extension.getURL('mytab.html'), 'active': true});});
