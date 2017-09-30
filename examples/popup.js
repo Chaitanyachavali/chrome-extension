b990a70afcc500bf6fbbc36a67af33e18a076b61
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2017-09-30 17:45:13 +0530

    Removing a tab on click

--- examples/popup.js
+++ examples/popup.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    window.location = "https://www.example.com";
-  }, false);
+    var checkPageButton = document.getElementById('clickTest');
+    checkPageButton.addEventListener('click', function() {
+        // https://stackoverflow.com/questions/16185044/get-the-urls-of-all-the-tabs-in-all-windows-using-chrome-extension
+        chrome.windows.getAll({
+            populate: true
+        }, function(windows) {
+            windows.forEach(function(window) {
+                window.tabs.forEach(function(tab) {
+                    // https://developer.chrome.com/extensions/tabs#method-remove
+                    // chrome.tabs.remove(214)
+                    console.log(tab);
+                });
+            });
+        });
+    }, false);
 }, false);
