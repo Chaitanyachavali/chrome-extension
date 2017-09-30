9de3c7068fe1fb2fce3a99c34a59c5d1c767eb07
Chaitanya Reddy <chavalichaithanyachinna@gmail.com>
2017-09-30 17:21:10 +0530

    try out examples

---
+++ examples/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+    window.location = "https://www.example.com";
+  }, false);
+}, false);
