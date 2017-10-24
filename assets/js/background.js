0a6628a5a59f1dfae3526c67283d076f6a548ac0
chaitanyachavali <chavalichaithanyachinna@gmail.com>
2017-10-24 06:12:08 +0530

    Listing all the context menus

--- assets/js/background.js
+++ assets/js/background.js
@@ -6,16 +6,86 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
-        title: 'MyTab',
-        id: 'menu1',
+        title: 'Display MyTab',
+        id: 'displayMyTab',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send all tabs to MyTab',
+        id: 'sendAllTabs',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+    	type: 'separator',
+        title: 'separator',
+        id: 'division1',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send only this tabs to MyTab',
+        id: 'onlyThisTab',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send all tabs except this tabs to MyTab',
+        id: 'exceptThisTab',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send all tabs on the right to MyTab',
+        id: 'fromLeftTab',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send all tabs on the left to MyTab',
+        id: 'fromRightTab',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+    	type: 'separator',
+        title: 'separator',
+        id: 'division2',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send all from all windows to MyTab',
+        id: 'allWindows',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Send all from all windows as groups to MyTab',
+        id: 'allWindowsAsGroups',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+    	type: 'separator',
+        title: 'separator',
+        id: 'division3',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Exclude this domain from MyTab',
+        id: 'exludeThis',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+    	type: 'separator',
+        title: 'separator',
+        id: 'division4',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Contact MyTab',
+        id: 'contact',
+        contexts: ['all'],
+    });
+    chrome.contextMenus.create({
+        title: 'Help',
+        id: 'help',
         contexts: ['all'],
     });
 });
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    if (info.menuItemId === "menu1") { 
-        console.log("Word " + info.selectionText + " was clicked.");
-		  chrome.tabs.create({  
-		    url: "http://www.google.com/search?q=" + info.selectionText,
-		  });
+    if (info.menuItemId === "displayMyTab") {
     }
 });
